b3d2ba9ffd7dafec3edd298a94941b0fad4e5167
Артём <morozov-f.morozov@yandex.ru>
2026-02-12 19:03:26 +0300

    Реализовал лёгенький калькулятор и проверил его через UnitTest

---
+++ TCalculator/TCalculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TCalculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TCalculator")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f7ab9bd4-dcc5-4a08-8b76-cb66001ed1b8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TCalculator/TCalculator/TCalculator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Calculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TCalculator
+{
+
+    [TestClass]
+
+    public class TCalculator
+    {
+        Calculatorr calculator = new Calculatorr();
+        [TestMethod]
+        public void TestSumma()
+        {
+            int x = 4;
+            int y = 8;
+
+            int expected = 12;
+            double action = calculator.Summa(x, y);
+            Assert.AreEqual(expected, action);
+        }
+
+        [TestMethod]
+        public void TestSubtraction()
+        {
+            int x = 20;
+            int y = 7;
+
+            int expected = 13;
+            double action = calculator.Subtraction(x, y);
+            Assert.AreEqual(expected, action);
+        }
+
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            int x = 5;
+            int y = 5;
+
+            int expected = 25;
+            double action = calculator.Multiplication(x, y);
+            Assert.AreEqual(expected, action);
+        }
+
+        [TestMethod]
+        public void TestDivideZero()
+        {
+            int x = 15;
+            int y = 0;
+
+            int expected = 0;
+            double action = calculator.Divide(x, y);
+            Assert.AreEqual(expected, action);
+        }
+
+        [TestMethod]
+        public void TestDivide()
+        {
+            int x = 15;
+            int y = 3;
+
+            int expected = 5;
+            double action = calculator.Divide(x, y);
+            Assert.AreEqual(expected, action);
+        }
+    }
+}
