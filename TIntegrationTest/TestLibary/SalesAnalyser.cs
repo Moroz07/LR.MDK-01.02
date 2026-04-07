e86708712fff005bd3e28f079ef9cd87f54e959a
Moroz <morozov-f.morozov@yandex.ru>
2026-04-07 09:35:26 +0300

    осталось реализовать тест на мой метод

---
+++ TIntegrationTest/TestLibary/SalesAnalyser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibary
+{
+    public class SalesAnalyser //посчитать товары, средняя выручка которых меньше х
+    {
+       public List<Product> CountProductPrice(List<Product> Allproduct, int x)
+        {
+            List<Product> product = new List<Product>();
+            double allrevenue = 0;
+            foreach (var p in Allproduct)
+            {
+                allrevenue = (int)p.Price * (int)p.Count;
+                if(allrevenue > x)
+                {
+                    product.Add(p);
+                }
+                allrevenue = 0;
+            }
+
+            return product;
+        }
+        
+    }
+}
